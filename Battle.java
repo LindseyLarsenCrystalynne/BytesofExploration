3ee062c2812042b66f4857c3deb4d9afde983587
Waterslullaby <crystal123456789q@gmail.com>
2017-05-16 08:55:07 -0500

    updated playerAttack()
    still needs an enemy taken from the group of enemies

--- Battle.java
+++ Battle.java
@@ -1,49 +1,47 @@
 import java.util.*;
+
+
 public class Battle
 {
 	public void start()
-  {
-  	boolean fight = true;
-  	System.out.println("⚔=⚔=⚔=⚔=⚔=Fight=⚔=⚔=⚔=⚔=⚔");
-    Scanner scan = new Scanner(System.in);
-    while(fight)
-    {
-    	System.out.println("Would you like to : Attack -- Defend -- Skill -- Item -- Run");
-      switch(scan.nextLine())
-      {
-      	case "Fight": 
-        	System.out.println("Which type of attack: Physical -- Magic");
-          switch(scan.nextLine())
-          {
-          	case "Physical":
-            playerAttack();
-            break;
-            
-            case "Magic":
-            //playerMAttack();
-            break;
-          }
-          break;
-          
-        case "Defend":
-        	System.out.println("You're defending!");
-         // playerDefend();
-        
-      }
-    }
-  }
+	{
+		boolean fight = true;
+		System.out.println("⚔=⚔=⚔=⚔=⚔=Fight=⚔=⚔=⚔=⚔=⚔");
+		Scanner scan = new Scanner(System.in);
+		while (fight) 
+		{
+			System.out.println("Would you like to : Fight -- Defend -- Skill -- Item -- Run");
+			switch (scan.nextLine())
+			{
+			case "Fight":
+				System.out.println("\nWhich type of attack: Physical -- Magic");
+				switch (scan.nextLine())
+				{
+				case "Physical":
+					System.out.println(playerAttack());
+					break;
 
-    
-  
-  
-  
-  public String playerAttack()
-  {
-  	Player p = new Player();
-  	Enemies e = new Enemies();
-  	
-  	int newHP = e.getCurHealth() - p.getAttack();
-  	e.setCurHealth(newHP);
-  	return "You attacked the enemy for " + p.getAttack() + " damage! The enemy now has " + newHP + "health.";
-  }
+				case "Magic":
+					// playerMAttack();
+					break;
+				}
+				break;
+
+			case "Defend":
+				System.out.println("You're defending!");
+				// playerDefend();
+
+			}
+		}
+	}
+
+	public String playerAttack()
+	{
+		Player p = new Player();
+		//need to set which enemy here
+
+		int newHP = e.getCurHealth() - p.getAttack();
+		e.setCurHealth(newHP);
+		return "\nYou attacked the enemy for " + p.getAttack() + " damage! The enemy now has " + newHP + " health.";
+	}
 }

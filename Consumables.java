d85a375ce586e2ad44b93f95c1091e482c60bcb8
Vallecillo <VL395264@lisd.net>
2017-05-16 08:55:18 -0500

    Added reusables and consumables

---
+++ Consumables.java
@@ -0,0 +1,29 @@
+public class Consumables
+{
+  public void useRemove()
+  {
+  
+  }
+  
+  public void heal()
+  {
+  
+  }
+  
+  public void use()
+  {
+  }
+  
+  public void damage()
+  {
+	}
+
+  public void mana()
+  {
+  }
+  
+  public void status()
+  {
+  
+  }
+}
---
+++ Reusables.java
@@ -0,0 +1,26 @@
+public class Reusable extends Items
+{
+  public void heal()
+  {
+  
+  }
+  
+  public void use()
+  {
+  }
+  
+  public void damage()
+  {
+  }
+
+  public void mana()
+  {
+  }
+  
+  public void status()
+  {
+  
+  }
+  
+  
+}

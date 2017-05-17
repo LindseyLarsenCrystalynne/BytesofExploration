77abaaf1e5686892d4b1096520633318e9c37f02
Waterslullaby <crystal123456789q@gmail.com>
2017-05-17 09:17:10 -0500

    Added Constructors

--- Enemies.java
+++ Enemies.java
@@ -1,121 +1,182 @@
 public class Enemies
+{
+	private int maxHealth;
+	private int curHealth;
+	private int maxMana;
+	private int curMana;
+	private int defense;
+	private int magicResistence;
+	private int attack;
+	private int magic;
+	private int dexterity;
+	private int exp;
+	private String name;
+	private int lvl;
+	private int money;
+	
+
+	
+	public Enemies()
 	{
-		private int maxHealth;
-    private int curHealth;
-    private int maxMana;
-    private int curMana;
-    private int defense;
-    private int magicResistence;
-    private int attack;
-    private int magic;
-    private int dexterity;
-    private int exp;
-    private String name;
-    private int lvl;
-    private int money;
-    
-	public int getMaxHealth() 
-    	{
+		maxHealth = 10;
+		curHealth = 10;
+		maxMana = 10;
+		curMana = 10;
+		defense = 4;
+		magicResistence = 4;
+		attack = 4;
+		magic = 4;
+		dexterity = 1;
+		exp = 5;
+		name = "Default";
+		lvl = 1;
+		money = 3;
+	}
+
+	public Enemies(String n, int mHealth, int mMana,int def,int mR,int attk,int mag,int dex,int exp,int lvl,int mon)
+	{
+		maxHealth = mHealth;
+		curHealth = mHealth;
+		maxMana = mMana; 
+		curMana = mMana;
+		defense = def;
+		magicResistence = mR; 
+		attack = attk;
+		magic = mag;
+		dexterity = dex;
+		this.exp = exp;
+		name = n;
+		this.lvl = lvl;
+		money = mon; 
+	}
+
+	public int getMaxHealth()
+	{
 		return maxHealth;
-   	}
-	public void setMaxHealth(int maxHealth) 
-    	{
+	}
+
+	public void setMaxHealth(int maxHealth)
+	{
 		this.maxHealth = maxHealth;
 	}
-	public int getCurHealth() 
+
+	public int getCurHealth()
 	{
 		return curHealth;
 	}
-	public void setCurHealth(int curHealth) 
-    	{
+
+	public void setCurHealth(int curHealth)
+	{
 		this.curHealth = curHealth;
 	}
-	public int getMaxMana() 
-    	{
+
+	public int getMaxMana()
+	{
 		return maxMana;
 	}
-	public void setMaxMana(int maxMana) 
-    	{
+
+	public void setMaxMana(int maxMana)
+	{
 		this.maxMana = maxMana;
 	}
-	public int getCurMana() 
-    	{
+
+	public int getCurMana()
+	{
 		return curMana;
 	}
-	public void setCurMana(int curMana) 
-    	{
+
+	public void setCurMana(int curMana)
+	{
 		this.curMana = curMana;
 	}
-	public int getDefense() 
-    	{
+
+	public int getDefense()
+	{
 		return defense;
 	}
-	public void setDefense(int defense) 
-    	{
+
+	public void setDefense(int defense)
+	{
 		this.defense = defense;
 	}
-	public int getMagicResistence() 
-    	{
+
+	public int getMagicResistence()
+	{
 		return magicResistence;
 	}
-	public void setMagicResistence(int magicResistence) 
-    	{
+
+	public void setMagicResistence(int magicResistence)
+	{
 		this.magicResistence = magicResistence;
 	}
-	public int getAttack() 
-    	{
+
+	public int getAttack()
+	{
 		return attack;
 	}
-	public void setAttack(int attack) 
-    	{
+
+	public void setAttack(int attack)
+	{
 		this.attack = attack;
 	}
-	public int getMagic() 
-    	{
+
+	public int getMagic()
+	{
 		return magic;
 	}
-	public void setMagic(int magic) 
-    	{
+
+	public void setMagic(int magic)
+	{
 		this.magic = magic;
 	}
-	public int getDexterity() 
-    	{
+
+	public int getDexterity()
+	{
 		return dexterity;
 	}
-	public void setDexterity(int dexterity) 
-    	{
+
+	public void setDexterity(int dexterity)
+	{
 		this.dexterity = dexterity;
 	}
-	public int getExp() 
-    	{
+
+	public int getExp()
+	{
 		return exp;
 	}
-	public void setExp(int exp) 
-    	{
+
+	public void setExp(int exp)
+	{
 		this.exp = exp;
 	}
-	public String getName() 
-    	{
+
+	public String getName()
+	{
 		return name;
 	}
-	public void setName(String name) 
-    	{
+
+	public void setName(String name)
+	{
 		this.name = name;
 	}
-	public int getLvl() 
-   	{
+
+	public int getLvl()
+	{
 		return lvl;
 	}
-	public void setLvl(int lvl) 
-    	{
+
+	public void setLvl(int lvl)
+	{
 		this.lvl = lvl;
 	}
-	public int getMoney() 
-    	{
+
+	public int getMoney()
+	{
 		return money;
 	}
-	public void setMoney(int money) 
-    	{
+
+	public void setMoney(int money)
+	{
 		this.money = money;
 	}
-	}
+}

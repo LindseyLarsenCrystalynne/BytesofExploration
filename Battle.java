3e2d3dab570565811fc169ec23b9f395cf4e384f
Waterslullaby <crystal123456789q@gmail.com>
2017-05-18 08:44:59 -0500

    working on skills

--- Battle.java
+++ Battle.java
@@ -3,14 +3,14 @@ import java.util.*;
 public class Battle
 {
 	ArrayList<Enemies> enemyList = new ArrayList<Enemies>();
-	
+
 	int enemy;
-	
+
 	public Battle(int enemyNumber)
 	{
 		enemy = enemyNumber;
 	}
-	
+
 	public void start()
 	{
 		enemyList.add(new Enemies());
@@ -36,13 +36,13 @@ public class Battle
 			 		{
 			 			case "Physical":
 			 			{
-			 				System.out.println(playerAttack());
+			 				System.out.println(playerAttack(1));
 			 				break;
 			 			}
 
 			 			case "Magic":
 			 			{
-			 				System.out.println(playerMAttack());
+			 				System.out.println(playerMAttack(1));
 			 				break;
 			 			}
 			 		}
@@ -58,26 +58,33 @@ public class Battle
 			 	
 			 	case "Skill":
 			 	{
+			 		Archer p = new Archer();
+			 		switch(p.getRole())
+			 		{
 			 		case "Archer":
 			 		{
 			 			System.out.println("\nWhich type of skill?\n");
-			 			System.out.println("[Shatter Arrow] [Sprint]");
+			 			System.out.println(" [Shatter Arrow] [Sprint]");
 			 			System.out.println("[Fire Arrow] [Arrow Storm]");
 			 			System.out.println("      [Ice Arrow]");
+			 			
+			 			break;
 			 		}
-			 		
+
 			 		case "Berserker":
 			 		{
 			 			System.out.println("\nWhich type of skill?\n");
 			 			System.out.println("[Power Slam] [Charge]");
-			 			System.out.println("[Rage] [Blood Lust]");
+			 			System.out.println(" [Rage] [Blood Lust]");
+			 			break;
 			 		}
 			 		
 			 		case "Knight":
 			 		{
 			 			System.out.println("\nWhich type of skill?\n");
 			 			System.out.println("[Heavy Slam] [Call Horse]");
-			 			System.out.println("[Bulk Up]");
+			 			System.out.println("       [Bulk Up]");
+			 			break;
 			 		}
 			 		
 			 		case "Mage":
@@ -86,86 +93,97 @@ public class Battle
 			 			System.out.println("[Fire Ball] [Hail]");
 			 			System.out.println("[Lightning] [Tsunami]");
 			 			System.out.println("[Boulder]");
+			 			break;
 			 		}
 			 		
 			 		case "Warrior":
 			 		{
 			 			System.out.println("\nWhich type of skill?\n");
 			 			System.out.println("[Strike] [Strength]");
+			 			break;
 			 		}
 			 		
 			 		default:
 			 		{
 			 			System.out.println("Invalid selection.");
+			 			break;
 			 		}
 			 	}
-			 	
-			 	default:
-			 	{
-			 		System.out.println("Invalid selection.");
 			 	}
 			}
-			System.out.println("test");
 			enemyTurn();
 		}
 	}
-
-	public String playerAttack()
+	
+	public String playerSkill(int damage, String skill)
 	{
 		Player p = new Player();
 
-		int newHP = enemyList.get(enemy).getCurHealth() - p.getAttack();
+		int newHP = (int) (enemyList.get(enemy).getCurHealth() - damage);
+		enemyList.get(enemy).setCurHealth(newHP);
+		return "\nYou attacked the enemy with " + skill + " for " + p.getAttack() + " damage! The enemy now has " + newHP + " health.";
+	}
+
+	public String playerAttack(double scale)
+	{
+		Player p = new Player();
+
+		int newHP = (int) (enemyList.get(enemy).getCurHealth() - (p.getAttack() * scale));
 		enemyList.get(enemy).setCurHealth(newHP);
 		return "\nYou attacked the enemy for " + p.getAttack() + " damage! The enemy now has " + newHP + " health.";
 	}
 
-	public String enemyAttack()
+	public String enemyAttack(double scale)
 	{
 		Player p = new Player();
 
-		int newHP = p.getCurHealth() - enemyList.get(enemy).getAttack();
+		int newHP = (int) (p.getCurHealth() - (enemyList.get(enemy).getAttack() * scale));
 		p.setCurHealth(newHP);
-		return "\nThe enemy attacked you for " + enemyList.get(enemy).getAttack() + " damage! You now have " + newHP + " health.";
+		return "\nThe enemy attacked you for " + enemyList.get(enemy).getAttack() + " damage! You now have " + newHP
+				+ " health.";
 	}
 
-	public String playerMAttack()
+	public String playerMAttack(double scale)
 	{
 		Player p = new Player();
-		
-		int newHP = enemyList.get(enemy).getCurHealth() - p.getMagic();
+
+		int newHP = (int) (enemyList.get(enemy).getCurHealth() - (p.getMagic() * scale));
 		enemyList.get(enemy).setCurHealth(newHP);
 		return "\nYou attacked the enemy for " + p.getMagic() + " damage! The enemy now has " + newHP + " health.";
 	}
 
-	public String enemyMAttack()
+	public String enemyMAttack(double scale)
 	{
 		Player p = new Player();
 
-		int newHP = p.getCurHealth() - enemyList.get(enemy).getMagic();
+		int newHP = (int) (p.getCurHealth() - (enemyList.get(enemy).getMagic() * scale));
 		p.setCurHealth(newHP);
-		return "\nThe enemy attacked you for " + enemyList.get(enemy).getMagic() + " damage! You now have " + newHP + " health.";
+		return "\nThe enemy attacked you for " + enemyList.get(enemy).getMagic() + " damage! You now have " + newHP
+				+ " health.";
 	}
 
 	public void enemyTurn()
 	{
-		
+
 		Player p = new Player();
-		
-		if (p.getMaxHealth() < 30) 
+
+		if (p.getMaxHealth() < 30) // if the player has less than 30 maximum
+									// health
 		{
-			if (((double) p.getCurHealth() / p.getMaxHealth()) <= .25)
-				System.out.println(enemyAttack());
-			else if ((((double) Math.random()) * 10) > 8) 
-				System.out.println("filler for enemy skill");
+			if (((double) p.getCurHealth() / p.getMaxHealth()) <= .25) // 
+				System.out.println(enemyAttack(1)); // a basic attack will be
+													// used
+			else if ((((double) Math.random()) * 25) > 24) // else at a 1/25
+															// chance the enemy
+															// will hit a crit
+				System.out.println("It was a crit!" + enemyAttack(1.5));
 			else
-				System.out.println(enemyAttack());
-		}
-		else if(((double) p.getCurHealth() / p.getMaxHealth()) <= .1)
-			System.out.println(enemyAttack());
-		else if ((((double) Math.random()) * 10) > 8) 
-			System.out.println("filler for enemy skill");
+				System.out.println(enemyAttack(1));
+		} else if (((double) p.getCurHealth() / p.getMaxHealth()) <= .1)
+			System.out.println(enemyAttack(1));
+		else if ((((double) Math.random()) * 10) > 8)
+			System.out.println("It was a crit!" + enemyAttack(1.5));
 		else
-			System.out.println(enemyAttack());
+			System.out.println(enemyAttack(1));
 	}
 }
-

0effe80cd13545a1b957bc790debe07f60bbc801
Waterslullaby <crystal123456789q@gmail.com>
2017-05-22 08:47:54 -0500

    Update TheRPGGame.java

--- TheRPGGame.java
+++ TheRPGGame.java
@@ -2,14 +2,13 @@ import java.util.*;
 
 public class TheRPGGame
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws InterruptedException
   {
   	System.out.println("The RPG Game");
     System.out.println("By Crystalynne Pham, Larsen Vallecillo, and Lindsey Truong");
     
     RPG r = new RPG();
-    r.Name();
-    r.Load();
+    
     r.mainMenu();
 	}
 }

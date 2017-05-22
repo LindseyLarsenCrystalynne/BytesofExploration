72825eb63675bc7e1ef0df906af40fcb31d4e224
Waterslullaby <crystal123456789q@gmail.com>
2017-05-21 20:18:47 -0500

    Added constructor

--- Consumables.java
+++ Consumables.java
@@ -1,25 +1,44 @@
-public class Consumables
+public class Consumables extends Items
 {
-  public void useRemove()
+	String name;
+	int price;
+	int heal;
+	int damage;
+	int mana;
+
+  public Consumables(String n, int p, int h, int d, int m)
   {
-  
+	  super(n,p);
   }
-  
+
   public void heal()
   {
-  
+	  Player p = new Player();
+	  
+	  if((p.getCurHealth() + heal) > p.getMaxHealth())
+	  {
+		  p.setCurHealth(p.getMaxHealth());
+	  }
   }
   
   public void use()
   {
+	  
   }
   
   public void damage()
   {
+	  
 	}
 
   public void mana()
   {
+	  Player p = new Player();
+	  
+	  if((p.getCurMana() + mana) > p.getMaxMana())
+	  {
+		  p.setCurMana(p.getMaxMana());
+	  }
   }
   
   public void status()

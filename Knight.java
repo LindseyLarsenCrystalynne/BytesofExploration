cc91fa8ce66f52b2ca010639e00cd49441b12b67
Waterslullaby <crystal123456789q@gmail.com>
2017-05-24 21:58:56 -0500

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -1,16 +1,22 @@
 public class Knight extends Player
 {
-  public int heavySlam()
-  {
-	  int damage = (int)(getLvl() * (Math.random() * 2) * getDefense());
-	  
-	  return damage;
-  }
-  
-  public int shieldBash()
-  {
-	  int damage = (int)((getLvl() * .75) * getDefense());
-	 
-	  return damage;
-  }
+	public int heavySlam()
+	{
+		int damage = (int) (getLvl() * (Math.random() * 2) * getDefense());
+
+		if (getLvl() == 0)
+			damage = 2;
+
+		return damage;
+	}
+
+	public int shieldBash()
+	{
+		int damage = (int) ((getLvl() * .75) * getDefense());
+
+		if (getLvl() == 0)
+			damage = 2;
+
+		return damage;
+	}
 }

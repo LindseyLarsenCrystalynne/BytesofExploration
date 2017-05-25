c5e2deddd60a5d45d9cd9da75ec4bd97b7f036b0
Waterslullaby <crystal123456789q@gmail.com>
2017-05-24 19:58:57 -0500

    added if lvl 0

--- Archer.java
+++ Archer.java
@@ -2,25 +2,41 @@ public class Archer extends Player
 {
 	public int shatterArrow()
 	{
-		int damage = (int)(getLvl() * .75 * getDexterity());
+		int damage = (int) (getLvl() * .75 * getDexterity());
+		
+		if (getLvl() == 0)
+			damage = 2;
+		
 		return damage;
 	}
 
 	public int fireArrow()
 	{
-		int damage = (int)(getLvl() * (Math.random() * 2) * getDexterity());
+		int damage = (int) (getLvl() * (Math.random() * 2) * getDexterity());
+		
+		if (getLvl() == 0)
+			damage = 2;
+		
 		return damage;
 	}
 
 	public int arrowStorm()
 	{
-		int damage = (int)(getLvl() * .55 * getDexterity());
+		int damage = (int) (getLvl() * .55 * getDexterity());
+		
+		if (getLvl() == 0)
+			damage = 2;
+		
 		return damage;
 	}
 
 	public int iceArrow()
 	{
-		int damage = (int)(getLvl() * (Math.random() * 1.75) * getDexterity());
+		int damage = (int) (getLvl() * (Math.random() * 1.75) * getDexterity());
+		
+		if (getLvl() == 0)
+			damage = 2;
+		
 		return damage;
 	}
 }

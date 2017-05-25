1cacdd0e16020957c2428c673fefbfe285560b40
Waterslullaby <crystal123456789q@gmail.com>
2017-05-24 19:42:07 -0500

    Fixing scalings

--- Mage.java
+++ Mage.java
@@ -2,25 +2,25 @@ public class Mage extends Player
 {
 	public int fireBall()
 	{
-		int damage = (int)(getLvl() * 1.25 * getDexterity());
+		int damage = (int)(getLvl() * 1.25 * getMagic());
 		return damage;
 	}
 
 	public int hail()
 	{
-		int damage = (int)(getLvl() * (Math.random() * 1.5) * getMagicResistence());
+		int damage = (int)(getLvl() * (Math.random() * 1.5) * getMagic());
 		return damage;
 	}
 
 	public int lightning()
 	{
-		int damage = (int)(getLvl() * (Math.random() * 2) * getMagicResistence());
+		int damage = (int)(getLvl() * (Math.random() * 2) * getMagic());
 		return damage;
 	}
 
 	public int tsunami()
 	{
-		int damage = (int)(getLvl() * 1.50 * getDexterity());
+		int damage = (int)(getLvl() * 1.50 * getMagic());
 		return damage;
 	}
 }

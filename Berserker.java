d51a4c9ac55f66014798aa7676e2ba5aba87c0fc
Waterslullaby <crystal123456789q@gmail.com>
2017-05-24 21:56:36 -0500

    Update Berserker.java

--- Berserker.java
+++ Berserker.java
@@ -1,20 +1,32 @@
 public class Berserker extends Player
 {
-	public int powerSlam() 
+	public int powerSlam()
 	{
-		int damage = (int)(getLvl() * .95 * getAttack());
+		int damage = (int) (getLvl() * .95 * getAttack());
+
+		if (getLvl() == 0)
+			damage = 2;
+
 		return damage;
 	}
 
 	public int charge()
 	{
-		int damage = (int)(getLvl() * (Math.random() * 2) * getAttack());
+		int damage = (int) (getLvl() * (Math.random() * 2) * getAttack());
+
+		if (getLvl() == 0)
+			damage = 2;
+
 		return damage;
 	}
 
 	public int rage()
 	{
-		int damage = (int)((getLvl() * .55) * 2) * getAttack();
+		int damage = (int) ((getLvl() * .55) * 2) * getAttack();
+
+		if (getLvl() == 0)
+			damage = 2;
+
 		return damage;
 	}
 }

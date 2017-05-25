55b9aef1af3f3e1bb1a1cb8858669afd26ca180d
Waterslullaby <crystal123456789q@gmail.com>
2017-05-24 19:42:37 -0500

    Fixed scalings
    and added one more attack ability

--- Knight.java
+++ Knight.java
@@ -2,7 +2,15 @@ public class Knight extends Player
 {
   public int heavySlam()
   {
-	  int damage = (int)(getLvl() * (Math.random() * 2) * getMagicResistence());
+	  int damage = (int)(getLvl() * (Math.random() * 2) * getDefense());
+	  
+	  return damage;
+  }
+  
+  public int shieldBash()
+  {
+	  int damage = (int)((getLvl() * .75) * getDefense());
+	 
 	  return damage;
   }
 }

71db658641834517ebf23fe41f52dfc44c1bc1d7
Waterslullaby <crystal123456789q@gmail.com>
2017-05-24 21:59:11 -0500

    Update Mage.java

--- Mage.java
+++ Mage.java
@@ -2,25 +2,41 @@ public class Mage extends Player
 {
 	public int fireBall()
 	{
-		int damage = (int)(getLvl() * 1.25 * getMagic());
+		int damage = (int) (getLvl() * 1.25 * getMagic());
+
+		if (getLvl() == 0)
+			damage = 2;
+
 		return damage;
 	}
 
 	public int hail()
 	{
-		int damage = (int)(getLvl() * (Math.random() * 1.5) * getMagic());
+		int damage = (int) (getLvl() * (Math.random() * 1.5) * getMagic());
+
+		if (getLvl() == 0)
+			damage = 2;
+
 		return damage;
 	}
 
 	public int lightning()
 	{
-		int damage = (int)(getLvl() * (Math.random() * 2) * getMagic());
+		int damage = (int) (getLvl() * (Math.random() * 2) * getMagic());
+
+		if (getLvl() == 0)
+			damage = 2;
+
 		return damage;
 	}
 
 	public int tsunami()
 	{
-		int damage = (int)(getLvl() * 1.50 * getMagic());
+		int damage = (int) (getLvl() * 1.50 * getMagic());
+
+		if (getLvl() == 0)
+			damage = 2;
+
 		return damage;
 	}
 }
